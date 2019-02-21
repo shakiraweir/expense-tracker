cc5f3abe194a961c951ed1c0e8c509817006b17a
Shakira Weir <shakira.weir87@gmail.com>
2019-02-21 12:46:58 -0500

    added state to expense

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './Expense.css'
-// The expense page can show ALL current expense entries AND also include a form to create a new entry. The new expense will populate above the nav bar.
+import axios from 'axios';
+
 
 class Expense extends Component {
+    // The revenue page can show ALL current revenue entries AND also include a form to create a new entry. The new revenue will populate above the nav bar.
+    constructor() {
+        super()
+        this.state = {
+            expense: [ ]
+        }
+    }
+            componentDidMount() {
+                axios.get('https://fun-budget-tool.herokuapp.com/expense')
+                    .then(res => {
+                        // console.log(res.data)
+                        this.setState({
+                            expense: res.data
+                        })
+                    })
+
+            }
+
+
+    
     render() {
+        // console.log(this.state.expense)
+        const { expense } = this.state;
+        const expenseList = 
+            expense.map(exp => {
+                return (
+                    <div className='expenseRevenue' key={exp.id}>
+                        <span className='expenseName'>{exp.name}</span>
+                        <span className='expenseAmount'> ${exp.amount}</span>    
+                    </div>
+                )
+        })
         return (
             <div>
                 <div className="links">
@@ -14,7 +45,7 @@ class Expense extends Component {
                         <Link to="/"><h3>Logout</h3>
                         </Link>
                     </nav>
-                     <div className="create">
+                    <div className="create">
                         <h3 className="subtitle">New Expense</h3>
                         <form className="new-entry">
                             <textarea cols="25" rows="1" placeholder="Title"></textarea>
@@ -23,22 +54,10 @@ class Expense extends Component {
                         <div>
                             <button className="btn-add"> +</button>  
                         </div>
-                        {/* <div>
-                            <button className="btn-update">update</button> 
-                            <button className="btn-delete"> delete</button>  
-                        </div> */}
-                        
-                    </div>
-{/*                      
-                    <footer className="total">
-	                    <div className="btn">
-	                        <button className="btn-left">Income</button>
-	                        <button className="btn-right">Expense</button>
-	                    </div>
-	                    <p>Current Balance</p>
-	                </footer> */}
+                        {expenseList}
                 </div>
             </div>
+        </div>
         );
     }
 }
